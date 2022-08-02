ee460943d7c06b991fd31998962199bb92da7204
alexpeev9 <alexpeev9@abv.bg>
2022-08-02 15:16:23 +0300

    :boom: Update starting point of API

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,15 +1,34 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import credentialsMiddleware from './middlewares/credentialsMiddleware';
+import mongoose from 'mongoose';
+import connectionString from './utils/connectionString';
+import corsOptionsConfig from './configs/corsOptionsConfig';
+import loggerMiddleware from './middlewares/loggerMiddleware';
 
 dotenv.config();
+const port = process.env.PORT || 5000;
 
 const app: Express = express();
-const port = process.env.PORT;
+app.use(credentialsMiddleware);
+app.use(cors(corsOptionsConfig));
+app.use(loggerMiddleware);
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(cookieParser());
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Test');
-});
+// TODO: app.use('/api', require('./routes'));
+app.all('*', (req: Request, res: Response) =>
+  res.status(404).json({ error: '404 Not Found' })
+);
 
-app.listen(port, () => {
-  console.log(`[server]: Server is running at https://localhost:${port}`);
-});
+mongoose
+  .connect(connectionString)
+  .then(() =>
+    app.listen(port, () => console.log(`Server is running on port ${port}`))
+  )
+  .catch((err) => {
+    console.log('Cannot connect to database.', err);
+  });
