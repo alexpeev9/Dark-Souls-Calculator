dd69b2a0f0e560535385da8c9cd975829103549d
alexpeev9 <alexpeev9@abv.bg>
2022-08-08 01:39:18 +0300

    :sparkles: Add Assets

---
+++ client/src/assets/images/icons/requirements/index.js
@@ -0,0 +1,6 @@
+import StrengthIcon from './strength_dark_souls.jpg';
+import DexterityIcon from './dexterity_dark_souls.jpg';
+import FaithIcon from './faith_dark_souls.jpg';
+import IntelligenceIcon from './intelligence_dark_souls.jpg';
+
+export { StrengthIcon, DexterityIcon, FaithIcon, IntelligenceIcon };
---
+++ client/src/assets/images/icons/stats/index.js
@@ -0,0 +1,5 @@
+import AttackTypeIcon from './attacktype_dark_souls.jpg';
+import DurabilityIcon from './durability_dark_souls.jpg';
+import WeightIcon from './weight_dark_souls.jpg';
+
+export { AttackTypeIcon, DurabilityIcon, WeightIcon };
