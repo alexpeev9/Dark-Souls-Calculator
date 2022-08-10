0871996cc4c34674f855fad6bca089cd043dab34
alexpeev9 <alexpeev9@abv.bg>
2022-08-10 14:42:52 +0300

    :bug: Add Prod and Url Api Base Url

--- client/src/store/api/index.ts
+++ client/src/store/api/index.ts
@@ -1,9 +1,14 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
+const baseUrl =
+  process.env.NODE_ENV === 'production'
+    ? `${process.env.REACT_APP_API_PROD_URL}/api`
+    : `http://localhost:${process.env.REACT_APP_API_DEV_PORT}/api`;
+
 const api = createApi({
   reducerPath: 'api',
   baseQuery: fetchBaseQuery({
-    baseUrl: 'http://localhost:5000/api',
+    baseUrl,
     credentials: 'include',
     // prepareHeaders: (headers, { getState }) => {
     //   headers.set('Content-Type', 'application/json');
@@ -11,7 +16,7 @@ const api = createApi({
     //   return headers;
     // },
   }),
-  tagTypes: ['Category', 'Weapon'],
+  tagTypes: ['Category', 'Weapon', 'User'],
   endpoints: (builder) => ({}),
 });
 
